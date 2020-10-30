5972fdf91242d4911cf581b016ecdcd9f30f92c1
Crew Spence <csspence123@gmail.com>
2020-10-30 05:23:05 -0500

    Wrote working solution.

--- codewars_my_head_is_at_the_wrong_end.js
+++ codewars_my_head_is_at_the_wrong_end.js
@@ -6,6 +6,6 @@ Save the animals by switching them back. You will be given an array which will h
 Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics - simples!
 */
 
-function fixTheMeerkat(arr) {
- //your code here 
+const fixTheMeerkat = (arr) => {
+ return [arr[2], arr[1], arr[0]];
 }
